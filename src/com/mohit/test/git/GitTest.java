88aa024bebc9a3d13131ffb01f8df1f59d4a23ee
imohitsaxena <iammohitsaxena@gmail.com>
2016-02-24 14:36:44 +0530

    Feature Util commit

--- src/com/mohit/test/git/GitTest.java
+++ src/com/mohit/test/git/GitTest.java
@@ -13,6 +13,8 @@ public class GitTest {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		
+		//Starting to work on feature util
 		System.out.println("This is version 1 for testing Git from Local to Git Push");
 		System.out.println("I am trying to make certain changes here. This is change 1 in version 1");
 
