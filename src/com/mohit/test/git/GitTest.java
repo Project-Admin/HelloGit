5dd60409911b705c6301a14c8439562b86990865
imohitsaxena <iammohitsaxena@gmail.com>
2016-02-24 14:09:35 +0530

    2nd commmit for a change

--- src/com/mohit/test/git/GitTest.java
+++ src/com/mohit/test/git/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
 	 */
 	public static void main(String[] args) {
 		System.out.println("This is version 1 for testing Git from Local to Git Push");
+		System.out.println("I am trying to make certain changes here. This is change 1 in version 1");
 
 	}
 
