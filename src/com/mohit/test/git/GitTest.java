0b589f61e3807ebae73e80f5e239393ce11d1601
imohitsaxena <iammohitsaxena@gmail.com>
2016-02-24 12:48:58 +0530

    First Commit

---
+++ src/com/mohit/test/git/GitTest.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.mohit.test.git;
+
+/**
+ * @author mohit.s
+ *
+ */
+public class GitTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("This is version 1 for testing Git from Local to Git Push");
+
+	}
+
+}
